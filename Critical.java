f9ebcd88c4cafea7aecc8f301ee9c2752520f427
anthonyprograms <anthony.williams@student.csulb.edu>
2016-10-11 19:46:49 -0700

    operation method to determine path of the thread

--- Critical.java
+++ Critical.java
@@ -5,20 +5,18 @@ public class Critical
     public static String[] pool = new String[125];
     public static String[] array = new String[100];
 
-    public static int randomLength() {
-        Random r = new Random();
-        int num = 5 + (int)(Math.random() * 5);
-        return num;
+    public static int randomLength(int min, int max) {
+        return min + (int)(Math.random() * max);
     }
 
     public static String randomString() {
         String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random r = new Random();
         String temp = "";
-        int stringLength = randomLength();
+        int stringLength = randomLength(5, 5);
 
         for (int i = 0; i < stringLength; i++) {
-            int location = (int)(Math.random() * letters.length());
+            int location = randomLength(0, letters.length());
             temp += Character.toString(letters.charAt(location));
         }
 
@@ -33,8 +31,40 @@ public class Critical
 
     public static void fillArray() {
         for(int i = 0; i < 100; i++){ 
-            int num = (int)(Math.random() * 125);
+            int num = randomLength(0, 125);
             array[i] = pool[num];
+        }
+    }
+
+    public static void runOperation() {
+        int num = randomLength(0, 2500);
+
+        if (num < 1900) {
+            System.out.println(occurences());
+        } else {
+            replace();
+        }
+    }
+
+    public static int occurences() {
+        String randomString = pool[randomLength(0,125)];
+        int count = 0;
+
+        for(int i = 0; i < array.length; i++) {
+            if (randomString.equals(array[i]))
+                count++;
+        }
+
+        return count;
+    }
+
+    public static void replace() {
+        String randomOne = pool[randomLength(0,125)];
+        String randomTwo = pool[randomLength(0,125)];
+
+        for(int i = 0; i < array.length; i++) {
+            if (randomOne.equals(array[i]))
+                array[i] = randomTwo;
         }
     }
 
@@ -42,8 +72,8 @@ public class Critical
     {
         fillPool();
         fillArray();
-        for(int i = 0; i < pool.length; i++){
-            System.out.println(pool[i]);
+        for (int i = 0; i < 500; i++) {
+            runOperation();
         }
     }
 }
